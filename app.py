8f5c9b8e206ca3b9605c186ddc26808a3cc4a64a
marcosotm <teniaquesermarcos@gmail.com>
2021-04-26 16:56:03 -0500

    third try

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import pandas_datareader.data as web
-import numpy as np
-import datetime
-
-import plotly
 import plotly.express as px
-import plotly.graph_objects as go
 
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash
-import dash_table
+
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import dash_auth
